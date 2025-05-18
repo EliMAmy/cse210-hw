f8f77bafe896f18bd59af38ecfe960591730b453
EliMAmy <rociomarapaza@gmail.com>
2025-05-17 19:47:11 -0500

    Comment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // ASk the user for their name and last name
         Console.Write("What is your first name? ");
         string first_name = Console.ReadLine();
         Console.Write("What is your last name? ");
