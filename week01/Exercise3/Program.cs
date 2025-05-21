ea8d2d37c008546a4e3b6eb75c31bcbd1019a6a1
EliMAmy <rociomarapaza@gmail.com>
2025-05-20 19:08:57 -0500

    Finished prep 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        string again = "yes";
+        int tries = 0;
+
+        while (again == "yes")
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            int yourGuess = int.Parse(guess);
+
+            if (yourGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+                tries = tries + 1;
+
+            }
+            else if (yourGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+                tries = tries + 1;
+
+            }
+            else if (yourGuess == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+                tries = tries + 1;
+                Console.WriteLine($"The number of tries is {tries}");
+                Console.Write("Do you want to play again? yes/no: ");
+                string playAgain = Console.ReadLine();
+                if (playAgain == "yes")
+                {
+                    Random randomGeneratornew = new Random();
+                    int newNumber = randomGeneratornew.Next(1, 50);
+                    tries = 0;
+                    again = "yes";
+                }
+                else
+                {
+                    again = "no";
+                }
+
+                }
+            }
+        Console.WriteLine($"The number of tries is {tries}");
     }
-}
+        
+    }
